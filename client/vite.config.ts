fb5066bfeaeb1ecdba858b372338d6fe05a6de6e
yoyojuls-dev <visbaljulius@gmail.com>
2026-03-26 13:58:36 +0800

    dedupe react instances

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   server: {
     port: 3000,
     proxy: {
